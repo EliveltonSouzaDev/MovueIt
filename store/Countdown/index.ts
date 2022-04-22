f28034d21aea548e471a276b4de754ef635bbda7
EliveltonSouzaDev <tst.elivelton@gmail.com>
2022-04-22 13:26:39 -0300

    feat: configurando o vuex da aplicação

--- store/Countdown/index.ts
+++ store/Countdown/index.ts
@@ -13,4 +13,17 @@ export const getters: Getters = {
 	seconds: (state: State): number => state.time % 60,
 };
 
-export const mutations: MutationsInterface = {};
+export const mutations: MutationsInterface = {
+	[Mutations.SET_TIME](state, newTime) {
+		state.time = newTime;
+	},
+	[Mutations.RESET_TIME](state) {
+		state.time = MINUTES * 60;
+	},
+	[Mutations.SET_IS_ACTIVE](state, isActive) {
+		state.isActive = isActive;
+	},
+	[Mutations.SET_HAS_COMPLETED](state, hasCompleted) {
+		state.hasCompleted = hasCompleted;
+	},
+};
